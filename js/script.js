bab64c607a4ca7b33c3e3d2e3180e17ab79e9fdb
Destiny Boone <destaj.db@gmail.com>
2023-01-06 13:00:35 -0500

    fading elements in and out

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-$(function() {
+$(function() { // must put jquery inside this line
   // jQuery goes here...
 
-  // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+  // fade outs the red box on page load
+  $(".red-box").fadeOut(2000); // value is in milliseconds
+  $(".green-box").fadeOut(2000);
+  $(".red-box").fadeIn(1000);
+  $(".red-box").fadeTo(1000, 0.5);
+  $(".blue-box").fadeToggle();
 });
