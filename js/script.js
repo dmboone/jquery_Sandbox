206736fa71e54cafb9e7c3a9df36c56a4e75ee07
Destiny Boone <destaj.db@gmail.com>
2023-01-18 15:00:18 -0500

    using the change event

--- js/script.js
+++ js/script.js
@@ -407,22 +407,38 @@ $(function() { // must put jquery inside this line
   // EVENTS II - FORMS
 
   // THE FOCUS AND BLUR EVENTS
-  let inputFields = $("input:text, input:password, textarea");
-  inputFields.focus(function(){
-    $(this).css("box-shadow", "0 0 4px #666");
-  });
+  // let inputFields = $("input:text, input:password, textarea");
+  // inputFields.focus(function(){
+  //   $(this).css("box-shadow", "0 0 4px #666");
+  // });
 
-  inputFields.blur(function(){
-    $(this).css("box-shadow", "none");
-  });
+  // inputFields.blur(function(){
+  //   $(this).css("box-shadow", "none");
+  // });
 
-  $("#name").blur(function(){
-    let text = $(this).val();
-    if(text.length < 3){
-      $(this).css("box-shadow", "0 0 4px #811");
+  // $("#name").blur(function(){
+  //   let text = $(this).val();
+  //   if(text.length < 3){
+  //     $(this).css("box-shadow", "0 0 4px #811");
+  //   }
+  //   else{
+  //     $(this).css("box-shadow", "0 0 4px #181");
+  //   }
+  // });
+
+  // USING THE CHANGE EVENT
+  $("#checkbox").change(function(){
+    let isChecked = $(this).is(":checked"); // .prop("checked")
+    if(isChecked){
+      $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #181");
     }
     else{
-      $(this).css("box-shadow", "0 0 4px #181");
+      $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #811");
     }
+  });
+
+  $("#selection").change(function(){
+    let selectedOption = $(this).find(":selected").text();
+    alert(selectedOption);
   });
 });
