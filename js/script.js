56fd03d2913c58858afc35831ca025d804bc3577
Destiny Boone <destaj.db@gmail.com>
2023-01-24 15:19:41 -0500

    fetching a server file with jquery

--- js/script.js
+++ js/script.js
@@ -455,150 +455,163 @@ $(function() { // must put jquery inside this line
   // });
 
   // CODING ACTIVITY: COMPLETE FORM VALIDATION ON SUBMIT
-  $("#form").submit(function(event){
-    let name = $("#name").val();
-    let password = $("#password").val();
-    let message = $("#message").val();
-    let checked = $("#checkbox").is(":checked");
+  // $("#form").submit(function(event){
+  //   let name = $("#name").val();
+  //   let password = $("#password").val();
+  //   let message = $("#message").val();
+  //   let checked = $("#checkbox").is(":checked");
 
-    validateNameField(name, event);
-    validatePasswordField(password, event);
-    validateMessageField(message, event);
-    validateCheckboxField(checked, event);
-  });
+  //   validateNameField(name, event);
+  //   validatePasswordField(password, event);
+  //   validateMessageField(message, event);
+  //   validateCheckboxField(checked, event);
+  // });
 
   // CODING ACTIVITY: FAST FEEBACK FORM
-  let form = $("#form");
-  enableFastFeedback(form);
+  // let form = $("#form");
+  // enableFastFeedback(form);
+
+  // AJAX WITH JQUERY
+  // $.get(), $.post(), $.ajax(), $.getJSON()
+
+  // FETCHING A SERVER FILE WITH JQUERY
+  // $.load()
+  $("#code").load("js/script.js");
+  $("#code").load("js/script.js", function(response, status){
+    if(status == "error"){
+      alert("Could not find file");
+    }
+    console.log(response);
+  });
 });
 
-function enableFastFeedback(formElement){
-  let nameInput = formElement.find("#name");
-  let passwordInput = formElement.find("#password");
-  let messageInput = formElement.find("#message");
-  let checkboxInput = formElement.find("#checkbox");
+// function enableFastFeedback(formElement){
+//   let nameInput = formElement.find("#name");
+//   let passwordInput = formElement.find("#password");
+//   let messageInput = formElement.find("#message");
+//   let checkboxInput = formElement.find("#checkbox");
 
-  nameInput.blur(function(event){
-    let name = $(this).val();
-    validateNameField(name, event);
+//   nameInput.blur(function(event){
+//     let name = $(this).val();
+//     validateNameField(name, event);
 
-    if(!isValidName(name)){
-      $(this).css({
-        "box-shadow": "0 0 4px #811", 
-        "border": "1px solid #600"
-      });
-    }
-    else{
-      $(this).css({
-        "box-shadow": "0 0 4px #181", 
-        "border": "1px solid #060"
-      });
-    }
-  });
+//     if(!isValidName(name)){
+//       $(this).css({
+//         "box-shadow": "0 0 4px #811", 
+//         "border": "1px solid #600"
+//       });
+//     }
+//     else{
+//       $(this).css({
+//         "box-shadow": "0 0 4px #181", 
+//         "border": "1px solid #060"
+//       });
+//     }
+//   });
 
-  passwordInput.blur(function(event){
-    let password = $(this).val();
-    validatePasswordField(password, event);
+//   passwordInput.blur(function(event){
+//     let password = $(this).val();
+//     validatePasswordField(password, event);
 
-    if(!isValidPassword(password)){
-      $(this).css({
-        "box-shadow": "0 0 4px #811", 
-        "border": "1px solid #600"
-      });
-    }
-    else{
-      $(this).css({
-        "box-shadow": "0 0 4px #181", 
-        "border": "1px solid #060"
-      });
-    }
-  });
+//     if(!isValidPassword(password)){
+//       $(this).css({
+//         "box-shadow": "0 0 4px #811", 
+//         "border": "1px solid #600"
+//       });
+//     }
+//     else{
+//       $(this).css({
+//         "box-shadow": "0 0 4px #181", 
+//         "border": "1px solid #060"
+//       });
+//     }
+//   });
 
-  messageInput.blur(function(event){
-    let message = $(this).val();
-    validateMessageField(message, event);
+//   messageInput.blur(function(event){
+//     let message = $(this).val();
+//     validateMessageField(message, event);
 
-    if(!isValidMessage(message)){
-      $(this).css({
-        "box-shadow": "0 0 4px #811", 
-        "border": "1px solid #600"
-      });
-    }
-    else{
-      $(this).css({
-        "box-shadow": "0 0 4px #181", 
-        "border": "1px solid #060"
-      });
-    }
-  });
+//     if(!isValidMessage(message)){
+//       $(this).css({
+//         "box-shadow": "0 0 4px #811", 
+//         "border": "1px solid #600"
+//       });
+//     }
+//     else{
+//       $(this).css({
+//         "box-shadow": "0 0 4px #181", 
+//         "border": "1px solid #060"
+//       });
+//     }
+//   });
 
-  checkboxInput.change(function(event){
-    let isChecked = $(this).is(":checked");
-    validateCheckboxField(isChecked, event);
+//   checkboxInput.change(function(event){
+//     let isChecked = $(this).is(":checked");
+//     validateCheckboxField(isChecked, event);
 
-    if(!isChecked){
-      $(this).add("label[for='cb']").css({
-        "box-shadow": "0 0 4px #811", 
-        "border": "1px solid #600"
-      });
-    }
-    else{
-      $(this).add("label[for='cb']").css({
-        "box-shadow": "0 0 4px #181", 
-        "border": "1px solid #060"
-      });
-    }
-  });
-}
+//     if(!isChecked){
+//       $(this).add("label[for='cb']").css({
+//         "box-shadow": "0 0 4px #811", 
+//         "border": "1px solid #600"
+//       });
+//     }
+//     else{
+//       $(this).add("label[for='cb']").css({
+//         "box-shadow": "0 0 4px #181", 
+//         "border": "1px solid #060"
+//       });
+//     }
+//   });
+// }
 
-function validateNameField(name, event){
-  if(!isValidName(name)){
-    $("#name-feedback").text("Please enter at least two characters");
-    event.preventDefault();
-  }
-  else{
-    $("#name-feedback").text("");
-  }
-}
+// function validateNameField(name, event){
+//   if(!isValidName(name)){
+//     $("#name-feedback").text("Please enter at least two characters");
+//     event.preventDefault();
+//   }
+//   else{
+//     $("#name-feedback").text("");
+//   }
+// }
 
-function validatePasswordField(password, event){
-  if(!isValidPassword(password)){
-    $("#password-feedback").text("The password should have at least 6 characters and contain a nunber");
-    event.preventDefault();
-  }
-  else{
-    $("#password-feedback").text("");
-  }
-}
+// function validatePasswordField(password, event){
+//   if(!isValidPassword(password)){
+//     $("#password-feedback").text("The password should have at least 6 characters and contain a nunber");
+//     event.preventDefault();
+//   }
+//   else{
+//     $("#password-feedback").text("");
+//   }
+// }
 
-function validateMessageField(message, event){
-  if(!isValidMessage(message)){
-    $("#message-feedback").text("Please enter a message");
-    event.preventDefault();
-  }
-  else{
-    $("#message-feedback").text("");
-  }
-}
+// function validateMessageField(message, event){
+//   if(!isValidMessage(message)){
+//     $("#message-feedback").text("Please enter a message");
+//     event.preventDefault();
+//   }
+//   else{
+//     $("#message-feedback").text("");
+//   }
+// }
 
-function validateCheckboxField(isChecked, event){
-  if(!isChecked){
-    $("#checkbox-feedback").text("Please agree to this");
-    event.preventDefault();
-  }
-  else{
-    $("#checkbox-feedback").text("");
-  }
-}
+// function validateCheckboxField(isChecked, event){
+//   if(!isChecked){
+//     $("#checkbox-feedback").text("Please agree to this");
+//     event.preventDefault();
+//   }
+//   else{
+//     $("#checkbox-feedback").text("");
+//   }
+// }
 
-function isValidName(name){
-  return name.length >= 2;
-}
+// function isValidName(name){
+//   return name.length >= 2;
+// }
 
-function isValidPassword(password){
-  return password.length >= 6 && /\d/.test(password);
-}
+// function isValidPassword(password){
+//   return password.length >= 6 && /\d/.test(password);
+// }
 
-function isValidMessage(message){
-  return message.trim() != "";
-}
+// function isValidMessage(message){
+//   return message.trim() != "";
+// }
